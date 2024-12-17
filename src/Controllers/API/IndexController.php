c9e7f3cdc1e983331c19893be7fa0ab01f8ac473
lilwizzi9 <lilwizzi9@gmail.com>
2024-12-17 19:54:53 +0100

    Fix

--- src/Controllers/API/IndexController.php
+++ src/Controllers/API/IndexController.php
@@ -6,17 +6,19 @@ use Internal\Http\Response;
 use Internal\Router\Route;
 use App\Models\Data\ApiResponseModel;
 use App\Middleware\DemoMiddleware;
+use Exception;
 use Internal\Attributes\AutoDocAttribute;
 use Internal\Attributes\AutoDocComment;
 use Internal\Attributes\RequestModelAttribute;
 use Internal\Middleware\RouteMiddleware;
+use Internal\Utils\GlobalLogger;
+use Throwable;
 
 #[AutoDocAttribute]
 #[RouteMiddleware(DemoMiddleware::class)]
 class IndexController
 {
     #[Route("GET","/")]
-    #[RequestModelAttribute(ApiResponseModel::class)]
     public function home(Request $request, Response $response){
 
         $response->setStatusCode(200);
@@ -28,6 +30,14 @@ class IndexController
     #[RequestModelAttribute(ApiResponseModel::class)]
     public function test(Request $request, Response $response){
         
+        $response->setStatusCode(200);
+        $response->json(new ApiResponseModel(true,"Welcome to simple php"));
+    }
+
+    #[AutoDocComment("Another Some Test Comment")]
+    #[Route("DELETE","/test1")]
+    public function test1(Request $request, Response $response){
+        GlobalLogger::log("Hello World");
         $response->setStatusCode(200);
         $response->json(new ApiResponseModel(true,"Welcome to simple php"));
     }
